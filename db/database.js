d8fdf8928f789480edf84f994057586130259da9
Murugula Sudheendra <msudheendra@opentext.com>
2022-10-19 19:11:50 +0530

    Html formating

--- db/database.js
+++ db/database.js
@@ -1,10 +1,6 @@
 const Client=require('pg').Client;
 const client = new Client({
-  user: 'intern',
-  host: '10.96.45.189',
-  database: 'intern_project_prms',
-  password: 'intern',
-  port: 5432,
+  
 })
 client.connect(function(err) {
     if (err) throw err;
