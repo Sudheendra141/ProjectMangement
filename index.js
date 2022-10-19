50f1be2c8929355d87153479a0ecadc21d17956b
Murugula Sudheendra <msudheendra@opentext.com>
2022-10-19 13:56:10 +0530

    added pagination and projectdetails

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var express=require('express');
 var path=require('path')
 var projectsRotes=require('./routes/projectDetails');
 var TeamRoutes=require('./routes/teamDetails');
+var bodyParser=require('body-parser');
 var app=express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res)=>
 {
     res.render('index');
 })
-console.log("hi 12")
 app.use('/ProjectDetails',projectsRotes);
-console.log("hi 13")
 app.use('/TeamDetails',TeamRoutes);
 app.listen(3000,console.log("Server startde successfully.. http://localhost:3000/"));
