fd094883f17c4b99d53b2524ed7c5d6432ef07bb
Murugula Sudheendra <msudheendra@opentext.com>
2022-10-19 19:16:25 +0530

    Ntng

--- db/database.js
+++ db/database.js
@@ -1,6 +1,10 @@
 const Client=require('pg').Client;
 const client = new Client({
-  
+  user: 'intern',
+  host: '10.96.45.189',
+  database: 'intern_project_prms',
+  password: 'intern',
+  port: 5432,
 })
 client.connect(function(err) {
     if (err) throw err;
